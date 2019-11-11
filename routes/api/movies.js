e646da9dcbbc873f244d4114635d76ea3a23cd92
ianodad <Ianodad@gmail.com>
2019-11-11 15:03:14 +0300

    working on auth

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -4,25 +4,50 @@ const Movies = require("../../services/movies");
 
 const router = express.Router();
 
+const Joi = require("joi");
+
 router.get("/", (req, res) => {
 	res.json(JSON.parse(Movies));
 });
 
 router.get("/:id", (req, res) => {
-	console.log(Movies.result);
-	const movie = Movies.result.some(
-		movie => movie.id === parseInt(req.params.id)
-	);
+	const { results } = JSON.parse(Movies);
+	const movie = results.some(movie => movie.id === parseInt(req.params.id));
 
 	!movie
 		? res.status(404).send("Movie not found")
 		: // send filter results
-		  res.send(Movie.filter(movie => movie.id === parseInt(req.params.id)));
+		  res.send(results.filter(movie => movie.id === parseInt(req.params.id)));
 });
 
 // router.post("/:id/comment", (req, res) => {
+// 	const { results } = JSON.parse(Movies);
+// 	const movie = results.some(movie => movie.id === parseInt(req.params.id));
+// 	if (!movie) return res.status(400).send("Invalid movie.");
+
+// 	const { error } = validateComment(req.body);
+// 	if (error) {
+// 		res.status(400).send(error.details[0].message);
+// 		return;
+// 	}
+
+// 	movie.comment = req.body.comment;
+// 	// movie.comments = [...req.body];
+
+// 	res.send(movie);
+// });
+
+// router.get("/:id/comments", (req, res) => {
 // 	console.log(Genre);
 // 	res.send(JSON.parse(Genre));
 // });
 
+function validateComment(comment) {
+	const schema = {
+		comment: Joi.string(),
+		username: Joi.string().required()
+	};
+
+	return Joi.validate(comment, schema);
+}
 module.exports = router;
--- startup/routes.js
+++ startup/routes.js
@@ -7,6 +7,9 @@ module.exports = function(app) {
 		})
 	);
 
+	// Body Parser
+	app.use(express.json());
+
 	// API ROUTINGS
 	app.use("/api/users", require("../routes/api/users"));
 	app.use("/api/movies", require("../routes/api/movies"));
