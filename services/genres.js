18419879e408ea36ec223acdf3d5688590634b89
ianodad <ianodad@gmail.com>
2020-07-29 13:28:03 +0300

    This is to server

--- services/genres.js
+++ services/genres.js
@@ -3,12 +3,16 @@ const axios = require('axios').default;
 
 // const config = require("config");
 
- 
+
 const genre = async() => {
-	const {data} = await axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=0ac0a0ff34c0490450e8232fa50b2269&language=en-US');
+  try {
+    return await axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=0ac0a0ff34c0490450e8232fa50b2269&language=en-US');
+  } catch (e){
+    console.log(e)
+  } finally {
+    console.log('Clean up')
+  }
 	// console.log(response)
-	console.log('hi')
-	return(data)
 };
 
 
